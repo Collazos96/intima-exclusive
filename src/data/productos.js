73278653140afdabbea5ccc5fb44b1371d78aff5
Juan Felipe Collazos Cabrera <juanfecoca@hotmail.com>
2026-04-13 20:56:54 -0500

    Imagenes migradas a cloudflare

--- src/data/productos.js
+++ src/data/productos.js
@@ -15,11 +15,11 @@ export const productos = [
     nuevo: true,
     descripcion: `Una prenda hecha con amor en Colombia, pensada para resaltar tu belleza con comodidad y estilo. Confeccionada con encajes de la más alta calidad, suaves al tacto y delicados con tu piel. Cada diseño incluye detalles en herraje color oro, que aportan un toque de elegancia y exclusividad. Ideal para mujeres que valoran los detalles y se sienten seguras de sí mismas desde adentro.`,
     imagenes: [
-      'https://i.ibb.co/ccBFMkhZ/SET-CARMINA.jpg',
-      'https://i.ibb.co/SwHx4WP7/SET-CARMINA-1.jpg',
-      'https://i.ibb.co/xKBJWMjP/SET-CARMINA-2.jpg',
-      'https://i.ibb.co/nMCTTjCf/SET-CARMINA-3.jpg',
-      'https://i.ibb.co/MkjLTvYZ/SET-CARMINA-4.jpg',
+      'https://images.intimaexclusive.com/sets/SET-CARMINA.jpg',
+      'https://images.intimaexclusive.com/sets/SET-CARMINA-1.jpg',
+      'https://images.intimaexclusive.com/sets/SET-CARMINA-2.jpg',
+      'https://images.intimaexclusive.com/sets/SET-CARMINA-3.jpg',
+      'https://images.intimaexclusive.com/sets/SET-CARMINA-4.jpg',
     ],
     colores: [
       { nombre: 'Rojo', tallas: ['S', 'M', 'L', 'XL'] },
@@ -35,11 +35,11 @@ export const productos = [
     nuevo: true,
     descripcion: `Una prenda hecha con amor en Colombia, pensada para resaltar tu belleza con comodidad y estilo. Confeccionada con encajes de la más alta calidad, suaves al tacto y delicados con tu piel. Cada diseño incluye detalles en herraje color oro, que aportan un toque de elegancia y exclusividad. Ideal para mujeres que valoran los detalles y se sienten seguras de sí mismas desde adentro.`,
     imagenes: [
-      'https://i.ibb.co/LX5C1pX2/SET-JULIETTA.jpg',
-      'https://i.ibb.co/rf57xLcH/SET-JULIETTA-1.jpg',
-      'https://i.ibb.co/sdJvFpTh/SET-JULIETTA-2.jpg',
-      'https://i.ibb.co/k2JtcFQN/SET-JULIETTA-3.jpg',
-      'https://i.ibb.co/RKqyVKx/SET-JULIETTA-4.jpg',
+      'https://images.intimaexclusive.com/sets/SET-JULIETTA.jpg',
+      'https://images.intimaexclusive.com/sets/SET-JULIETTA-1.jpg',
+      'https://images.intimaexclusive.com/sets/SET-JULIETTA-2.jpg',
+      'https://images.intimaexclusive.com/sets/SET-JULIETTA-3.jpg',
+      'https://images.intimaexclusive.com/sets/SET-JULIETTA-4.jpg',
     ],
     colores: [
       { nombre: 'Azul', tallas: ['S', 'M'] },
